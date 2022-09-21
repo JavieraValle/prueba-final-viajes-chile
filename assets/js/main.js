4f43ac1cab872636387d5dfe97abc78ed7a02763
Javiera Valle <javi.valle.munoz@gmail.com>
2022-09-21 20:34:33 -0300

    Integración Smooth Scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,29 @@
 $(document).ready(function () {
 
+     $("a").click(function () {
+         var gato = this.hash
+
+         $("html, body").animate(
+             {
+                 scrollTop: $(gato).offset().top
+             },
+             800
+         )
+     })
+
+
+
     //Evento
 
-$("#enviar").click(function(){
+    $("#enviar").click(function () {
 
-    alert("El mensaje fue enviado correctamente...")
-})
+        alert("El mensaje fue enviado correctamente...")
+    })
 
+    //Tooltip Enable
 
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-//Tooltip Enable
-
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 })
